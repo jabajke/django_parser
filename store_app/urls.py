25fd387bbbba0d40a89400e5d721c992a5134d22
Pavel <aaamazafak@yandex.ru>
2021-11-16 12:56:33 +0300

    dynamic url added

--- store_app/urls.py
+++ store_app/urls.py
@@ -5,10 +5,11 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('', ShowItemView.as_view(), name='show_item'),
     path('index/', IndexView.as_view(), name='index'),
-    path('categories/', TemplateView.as_view(template_name='store_app/categories.html'), name='categories'),
-    path('product/', TemplateView.as_view(template_name='store_app/product.html'), name='product'),
-    path('checkout/', TemplateView.as_view(template_name='store_app/checkout.html'), name='checkout'),
-    path('contact/', TemplateView.as_view(template_name='store_app/contact.html'), name='contact'),
-    path('cart/', TemplateView.as_view(template_name='store_app/cart.html'), name='cart'),
+    path('categories/', CategoryView.as_view(), name='categories'),
+    path('categories/<str:i>', TemplateView.as_view(template_name='store_app/categories.html'), name='category_item'),
+    path('product/', ProductView.as_view(), name='product'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('contact/', ContactView.as_view(), name='contact'),
+    path('cart/', CartView.as_view(), name='cart'),
 
 ]
--- store_app/views.py
+++ store_app/views.py
@@ -23,3 +23,23 @@ class IndexView(TemplateView):
     for i in category:
         list_category.append(i['category'])
         extra_context = {'data': list_category}
+
+
+class CategoryView(IndexView):
+    template_name = 'store_app/categories.html'
+
+
+class ProductView(IndexView):
+    template_name = 'store_app/product.html'
+
+
+class ContactView(IndexView):
+    template_name = 'store_app/contact.html'
+
+
+class CartView(IndexView):
+    template_name = 'store_app/cart.html'
+
+
+class CheckoutView(IndexView):
+    template_name = 'store_app/checkout.html'
